b565b298700605dd6a5dc8f53338a919617c3c31
jihu <hyub1015@gmail.com>
2022-01-20 00:45:59 +0900

    project setting

---
+++ src/test/java/com/example/forjwebapp/ForJWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forjwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForJWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
